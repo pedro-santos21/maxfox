8dc468296e954cd1ec8e015d9de1a5619539c815
Pedro Santos <142705026+pedro-santos21@users.noreply.github.com>
2023-12-22 18:37:37 -0300

    fix typo

--- maxfox/app/page.tsx
+++ maxfox/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center bg-white py-8">
       <MaxFoxPfp />
       <h1 className="text-center text-3xl text-black drop-shadow-lg pt-1"><b>Max Fox</b></h1>
-      <p className="text-black font-light pt-1 pb-1">Entrepeneur, Developer, Thinker, Investor, Artist</p>
+      <p className="text-black font-light pt-1 pb-1">Entrepreneur, Developer, Thinker, Investor, Artist</p>
       <SocialMediaBanner />
       <div className="py-7">
         <QuoteBox text='The internet is full of pretentious asswipes.' author='Max Fox'/>
