9c9c5b3806ff1741ab2acacb6774c925498ca8a5
Pedro Santos <142705026+pedro-santos21@users.noreply.github.com>
2023-12-22 10:05:53 -0300

    initial setup + cool ass navbar

---
+++ maxfox/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link'
+
+const NavBar = () => {
+  return (
+    <nav className='justify-center flex place-content-center items-center p-4 sticky'>
+        <Link className='flex self-center items-center pt-2 text-lg hover:underline underline-offset-8 transition ease-in-out hover:scale-105' href='/'>Home</Link>
+        <Link href='/maxfox'>
+            <h1 className='flex text-4xl text-center self-center pr-8 pl-8 font-bold hover:scale-105'>Max Fox</h1>
+        </Link>
+        <Link className='flex self-center items-center pt-2 text-lg hover:underline underline-offset-8 transition ease-in-out hover:scale-105' href='/posts'>Posts</Link>
+    </nav>
+  );
+};
+
+export default NavBar;
